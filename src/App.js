a348c657dcf32ef2447b4d64a303095f3c220473
AnumEssani <56483258+AnumEssani@users.noreply.github.com>
2021-01-15 12:10:23 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {Component}from 'react';
+import DogImage from './DogImage'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dogImages: [ ]
+    }
+  }
+ componentDidMount(){
+   fetch('https://dog.ceo/api/breeds/image/random/3')
+   .then(res => res.json() ) 
+   .then(( data)=> {
+     this.setState({dogImages: data.message })
+   })
+ }
+
+
+  render() {
+    const dogList = this.state.dogImages.map((url,i) => {
+      return <DogImage key ={ i} url= {url}/>
+    })
+    return(
+      <div className= 'App'>
+        <h1>Dog Images</h1>
+        {dogList}
+      </div>
+    )
+  }
+
+}
+
+export default App;
---
+++ src/DogImage.js
@@ -0,0 +1,11 @@
+import React, {Component}from 'react';
+
+function DogImage(props) {
+    return(
+        <img src = {props.url}/>
+    )
+
+
+}
+
+export default DogImage;
